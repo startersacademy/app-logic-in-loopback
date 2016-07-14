f2442f22efb071afb96be475cf7f9d6e81a30901
Todd Bashor <todd@startersacademy.com>
2016-07-14 14:11:48 -0700

    Enhance boot script that provides / route

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,10 +1,20 @@
-module.exports = function(server) {
+module.exports = function(app) {
   'use strict'
-  var log = require('debug')('boot:root')
+  const log = require('debug')('boot:root')
   log('show status at /')
 
+  const pkg = require('../../package')
+  const started = new Date()
+
   // Install a `/` route that returns server status
-  var router = server.loopback.Router()
-  router.get('/', server.loopback.status())
-  server.use(router)
+  app.get('/', function(req, res) {
+    res.json({
+      app: pkg.name,
+      version: pkg.version,
+      env: process.env.NODE_ENV,
+      apiUrl: app.get('restApiRoot'),
+      started: started,
+      uptime: (Date.now() - Number(started)) / 1000
+    })
+  })
 }
