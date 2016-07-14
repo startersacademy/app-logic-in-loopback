1a0959dba04fb821fa0dbeb7b2785903187be7b6
Todd Bashor <todd@startersacademy.com>
2016-07-14 14:10:32 -0700

    Add boot script to autoupdate database

--- server/boot/autoupdate-database.js
+++ server/boot/autoupdate-database.js
@@ -8,7 +8,7 @@ module.exports = function(app) {
     ds.autoupdate(function(err){
       if (err) {
         console.error('problem creating or updating tables', err)
-        //throw err
+        throw err
       }
       console.log('created or updated tables')
       app.emit('ready', 'database')
